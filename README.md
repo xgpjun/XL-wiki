246b0e7d814b1c2a67c2fa0debf70c5c4600b83d
357004885@qq.com <357004885@qq.com>
2024-01-07 07:38:02 +0800

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+# 让Bukkit插件支持Folia
+![](https://github.com/PaperMC/Folia/raw/master/folia.png)
---
+++ docusaurus.config.js
@@ -0,0 +1,160 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'XgpLottery2',
+  tagline: 'Wiki',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://xgpjun.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/XL-wiki/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'xgpjun', // Usually your GitHub org/user name.
+  projectName: 'XL-wiki', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans', 'en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          editUrl:
+            'https://github.com/xgpjun/Folia-Tutorials/blob/main'
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Tutorials',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.svg',
+        },
+        hideOnScroll: false,
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          // 搜索框
+          {
+            type: 'search',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/xgpjun/Folia-Tutorials',
+            label: 'GitHub',
+            position: 'right',
+          },
+          // {
+          //   type: 'localeDropdown',
+          //   position: 'right',
+          // },
+        ],
+      },
+      // 底部链接
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: '开始',
+                to: '/intro',
+              },
+            ],
+          },
+          {
+            title: '联系方式',
+            items: [
+              {
+                label: 'QQ群',
+                href: 'https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=SY19EvEiFZvqx_mwGSvYn5N-NkgoiqBd&authKey=ch4cQrooW0WGSG9IlomgJH43qyhE8W8alImhOM%2FUbhcTSTb7f1nH%2Fq5k7KlhRhX8&noverify=0&group_code=851355982',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/pAA54hqX',
+              },
+              {
+                label: 'MCBBS',
+                href: 'https://www.mcbbs.net/home.php?mod=space&uid=4042572',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/xgpjun/Folia-Tutorials',
+              },
+            ],
+          },
+        ],
+        // 底部版权信息
+        copyright: `Copyright © ${new Date().getFullYear()} 小钢炮君, Inc. Built with Docusaurus.`,
+      },
+      // 深浅主题
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      // 颜色随系统切换
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+    }),
+  
+    themes: [
+      [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        {
+          hashed: true,
+          language: ["en", "zh"],
+          highlightSearchTermsOnTargetPage: true,
+          explicitSearchResultPath: true,
+          indexBlog: false,
+          docsRouteBasePath: "/"
+        },
+      ],
+    ],
+};
+
+module.exports = config;
